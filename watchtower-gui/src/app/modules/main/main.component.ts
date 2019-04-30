35e6d71ea012a5fbecd5c659a18368f76ba771bb
tomas.crespo <tomas.crespo@salenda.es>
2019-04-30 14:29:33 +0200

    Add essential JS and CSS libraries. Add layout components. Start initial GUI draft.

---
+++ watchtower-gui/src/app/modules/main/main.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'wt-main',
+  templateUrl: './main.component.html',
+  styleUrls: ['./main.component.scss']
+})
+export class MainComponent {
+
+}
