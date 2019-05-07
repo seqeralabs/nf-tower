c412082490d49ce01f32ccd3256da878db1a8474
tomas.crespo <tomas.crespo@salenda.es>
2019-05-07 08:28:56 +0200

    Use Inject annotation in tests. Start using Spock mocks in tests

--- watchtower-service/src/test/groovy/io/seqera/watchtower/service/TraceServiceSpec.groovy
+++ watchtower-service/src/test/groovy/io/seqera/watchtower/service/TraceServiceSpec.groovy
@@ -1,8 +1,8 @@
 package io.seqera.watchtower.service
 
-import io.micronaut.context.ApplicationContext
-import io.micronaut.runtime.server.EmbeddedServer
+
 import io.micronaut.test.annotation.MicronautTest
+import io.micronaut.test.annotation.MockBean
 import io.seqera.watchtower.Application
 import io.seqera.watchtower.domain.Task
 import io.seqera.watchtower.domain.Workflow
@@ -10,25 +10,36 @@ import io.seqera.watchtower.pogo.enums.TraceType
 import io.seqera.watchtower.pogo.exceptions.NonExistingWorkflowException
 import io.seqera.watchtower.util.AbstractContainerBaseSpec
 import io.seqera.watchtower.util.DomainCreator
-import spock.lang.AutoCleanup
-import spock.lang.Shared
-import spock.util.mop.ConfineMetaClassChanges
+import spock.lang.IgnoreRest
+
+import javax.inject.Inject
 
 @MicronautTest(application = Application.class)
 class TraceServiceSpec extends AbstractContainerBaseSpec {
 
-    @Shared @AutoCleanup
-    EmbeddedServer embeddedServer = ApplicationContext.run(EmbeddedServer)
+    @Inject
+    WorkflowService workflowService
+    @MockBean(WorkflowService)
+    WorkflowService workflowService() {
+        Mock(WorkflowService)
+    }
 
-    @Shared
-    TraceService traceService = embeddedServer.applicationContext.getBean(TraceService)
+    @Inject
+    TaskService taskService
+    @MockBean(TaskService)
+    TaskService taskService() {
+        Mock(TaskService)
+    }
+
+    @Inject
+    TraceService traceService
 
 
-    @ConfineMetaClassChanges([WorkflowService])
+    @IgnoreRest
     void "process a successful workflow trace"() {
         given: "mock the workflow JSON processor to return a successful workflow"
         Workflow workflow = new DomainCreator().createWorkflow()
-        traceService.workflowService.metaClass.processWorkflowJsonTrace { Map workflowJson ->
+        workflowService.processWorkflowJsonTrace(_) >> {
             workflow
         }
 
@@ -41,12 +52,14 @@ class TraceServiceSpec extends AbstractContainerBaseSpec {
         !result.error
     }
 
-    @ConfineMetaClassChanges([WorkflowService])
+    @IgnoreRest
     void "process a workflow trace to start a new workflow with the same sessionId+runName combination of a previous one"() {
         given: "mock the workflow JSON processor to return a workflow with the same sessionId+runName combination as a previous one"
         Workflow workflow1 = new DomainCreator().createWorkflow()
         Workflow workflow2 = new DomainCreator(failOnError: false).createWorkflow(sessionId: workflow1.sessionId, runName: workflow1.runName)
-        traceService.workflowService.metaClass.processWorkflowJsonTrace { Map workflowJson ->
+
+        workflowService()
+        workflowService.processWorkflowJsonTrace(_) >> {
             workflow2
         }
 
@@ -59,13 +72,10 @@ class TraceServiceSpec extends AbstractContainerBaseSpec {
         !result.entityId
     }
 
-    @ConfineMetaClassChanges([WorkflowService])
     void "process a workflow trace to start workflow without submitTime"() {
         given: "mock the workflow JSON processor to return a workflow without submitTime"
         Workflow workflow = new DomainCreator(failOnError: false).createWorkflow(submitTime: null)
-        traceService.workflowService.metaClass.processWorkflowJsonTrace { Map workflowJson ->
-            workflow
-        }
+        workflowService.processWorkflowJsonTrace(_) >> workflow
 
         when: "process the workflow (we don't mind about the given JSON because the processor is mocked)"
         Map result = traceService.processWorkflowTrace(null)
@@ -76,11 +86,10 @@ class TraceServiceSpec extends AbstractContainerBaseSpec {
         !result.entityId
     }
 
-    @ConfineMetaClassChanges([WorkflowService])
     void "process a workflow trace, but throw a NonExistingWorkflow exception"() {
         given: "mock the workflow JSON processor to throw an exception"
         String exceptionMessage = 'message'
-        traceService.workflowService.metaClass.processWorkflowJsonTrace { Map workflowJson ->
+        workflowService.processWorkflowJsonTrace(_) >> {
             throw(new NonExistingWorkflowException(exceptionMessage))
         }
 
@@ -93,12 +102,9 @@ class TraceServiceSpec extends AbstractContainerBaseSpec {
         !result.entityId
     }
 
-    @ConfineMetaClassChanges([WorkflowService])
     void "process a workflow trace, but throw a generic exception"() {
         given: "mock the workflow JSON processor to throw an exception"
-        traceService.workflowService.metaClass.processWorkflowJsonTrace { Map workflowJson ->
-            throw(new RuntimeException())
-        }
+        workflowService.processWorkflowJsonTrace(_) >> { throw(new RuntimeException()) }
 
         when: "process the workflow (we don't mind about the given JSON because the processor is mocked)"
         Map result = traceService.processWorkflowTrace(null)
@@ -109,13 +115,10 @@ class TraceServiceSpec extends AbstractContainerBaseSpec {
         !result.entityId
     }
 
-    @ConfineMetaClassChanges([TaskService])
     void "process a successful task trace"() {
         given: "mock the task JSON processor to return a successful task"
         Task task = new DomainCreator().createTask()
-        traceService.taskService.metaClass.processTaskJsonTrace { Map taskJson ->
-            task
-        }
+        taskService.processTaskJsonTrace(_) >> task
 
         when: "process the task (we don't mind about the given JSON because the processor is mocked)"
         Map result = traceService.processTaskTrace(null)
@@ -126,13 +129,10 @@ class TraceServiceSpec extends AbstractContainerBaseSpec {
         !result.error
     }
 
-    @ConfineMetaClassChanges([TaskService])
     void "process a task without submit time"() {
         given: "mock the task JSON processor to return a task without submit time"
         Task task = new DomainCreator(failOnError: false).createTask(submitTime: null)
-        traceService.taskService.metaClass.processTaskJsonTrace { Map taskJson ->
-            task
-        }
+        taskService.processTaskJsonTrace(_) >> task
 
         when: "process the task (we don't mind about the given JSON because the processor is mocked)"
         Map result = traceService.processTaskTrace(null)
@@ -143,15 +143,12 @@ class TraceServiceSpec extends AbstractContainerBaseSpec {
         result.error == "Can't start or complete a non-existing task"
     }
 
-    @ConfineMetaClassChanges([TaskService])
     void "process a task with the same taskId of a previous one for the same workflow"() {
         given: "mock the task JSON processor to return a task with the same taskId of a previous one for the same workflow"
         Workflow workflow = new DomainCreator().createWorkflow()
         Task task1 = new DomainCreator().createTask(workflow: workflow)
         Task task2 = new DomainCreator(failOnError: false).createTask(workflow: workflow, taskId: task1.taskId)
-        traceService.taskService.metaClass.processTaskJsonTrace { Map taskJson ->
-            task2
-        }
+        taskService.processTaskJsonTrace(_) >> task2
 
         when: "process the task (we don't mind about the given JSON because the processor is mocked)"
         Map result = traceService.processTaskTrace(null)
@@ -161,6 +158,5 @@ class TraceServiceSpec extends AbstractContainerBaseSpec {
         !result.entityId
         result.error == "Can't submit a task which was already submitted"
     }
-
 
 }
