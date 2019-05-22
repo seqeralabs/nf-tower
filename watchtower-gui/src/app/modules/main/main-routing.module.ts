8f11fee77cfe3dd102a71cd8870bcf85cad0dd83
tomas.crespo <tomas.crespo@salenda.es>
2019-05-22 07:37:31 +0200

    Add mock login page and auth service

--- watchtower-gui/src/app/modules/main/main-routing.module.ts
+++ watchtower-gui/src/app/modules/main/main-routing.module.ts
@@ -1,8 +1,13 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import {WorkflowDetailComponent} from "./component/workflow-detail/workflow-detail.component";
+import {HomeComponent} from "./component/home/home.component";
+import {LoginComponent} from "./component/login/login.component";
+import {AuthGuard} from "./guard/auth.guard";
 
 const routes: Routes = [
+  {path: '',             component: HomeComponent, canActivate: [AuthGuard]},
+  {path: 'login',        component: LoginComponent},
   {path: 'workflow/:id', component: WorkflowDetailComponent},
 ];
 
