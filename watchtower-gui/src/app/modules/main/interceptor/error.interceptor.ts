8655ff2ef5a6fd06d25518dc2eb1775ec47f07c2
tomas.crespo <tomas.crespo@salenda.es>
2019-05-23 13:50:26 +0200

    Structure child routes for auth sections

--- watchtower-gui/src/app/modules/main/interceptor/error.interceptor.ts
+++ watchtower-gui/src/app/modules/main/interceptor/error.interceptor.ts
@@ -4,18 +4,23 @@ import {Observable, throwError} from 'rxjs';
 import {AuthService} from "../service/auth.service";
 import {catchError} from "rxjs/operators";
 import {Router} from "@angular/router";
+import {NotificationService} from "../service/notification.service";
 
 @Injectable()
 export class ErrorInterceptor implements HttpInterceptor {
 
-  constructor(private authService: AuthService, private router: Router) {}
+  constructor(private authService: AuthService, private notificationService: NotificationService, private router: Router) {}
 
   intercept(request: HttpRequest<any>, next: HttpHandler): Observable<HttpEvent<any>> {
     return next.handle(request).pipe(
       catchError(error => {
-        let authorizationErrorCodes: number[] = [401, 403];
+        const authorizationErrorCodes: number[] = [401, 403];
 
         if (authorizationErrorCodes.includes(error.status)) {
+          if (this.authService.isUserLoggedIn) {
+            this.notificationService.showErrorNotification('Session expired');
+          }
+
           this.authService.logout();
           this.router.navigate(['/logout'])
         }
