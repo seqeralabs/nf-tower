6ae150d603529460a6d73b6802e970c1f192c3ec
tomas.crespo <tomas.crespo@salenda.es>
2019-05-30 09:08:33 +0200

    Handle idle flowable timeout

--- watchtower-service/src/main/groovy/io/seqera/watchtower/service/ServerSentEventsServiceImpl.groovy
+++ watchtower-service/src/main/groovy/io/seqera/watchtower/service/ServerSentEventsServiceImpl.groovy
@@ -1,41 +1,74 @@
 package io.seqera.watchtower.service
 
+import groovy.util.logging.Slf4j
+import io.micronaut.context.annotation.Prototype
+import io.micronaut.context.annotation.Value
 import io.micronaut.http.sse.Event
 import io.reactivex.Flowable
+import io.reactivex.functions.Consumer
 import io.reactivex.processors.PublishProcessor
 import io.seqera.watchtower.pogo.exceptions.NonExistingFlowableException
 
-import javax.inject.Singleton
+import java.time.Duration
 import java.util.concurrent.ConcurrentHashMap
+import java.util.concurrent.TimeUnit
+import java.util.concurrent.TimeoutException
 
-@Singleton
+@Prototype
+@Slf4j
 class ServerSentEventsServiceImpl implements ServerSentEventsService {
 
     private Map<Long, PublishProcessor<Event>> flowableByIdCache = new ConcurrentHashMap()
 
+    @Value('${sse.idle.timeout:5m}')
+    Duration idleFlowableTimeout
 
     void createFlowable(Long id) {
+        log.info("Creating flowable: ${id}")
+
         flowableByIdCache[id] = PublishProcessor.create()
+        scheduleFlowableIdleTimeout(id)
     }
 
-    void publishUpdate(Long id, def data) {
+    void publishData(Long id, def data) {
+        log.info("Publishing data for flowable: ${id}")
         PublishProcessor hotFlowable = (PublishProcessor) getFlowable(id)
 
         hotFlowable.onNext(Event.of(data))
     }
 
     void completeFlowable(Long id) {
+        log.info("Completing flowable: ${id}")
         PublishProcessor hotFlowable = (PublishProcessor) getFlowable(id)
 
         hotFlowable.onComplete()
         flowableByIdCache.remove(id)
     }
 
+    private void scheduleFlowableIdleTimeout(Long id) {
+        Flowable flowable = getFlowable(id)
+
+        Flowable timeoutFlowable = flowable.timeout(idleFlowableTimeout.toMillis(), TimeUnit.MILLISECONDS)
+        timeoutFlowable.subscribe(
+                {
+                    log.info("Data published for flowable: ${id}")
+                } as Consumer,
+                { Throwable t ->
+                    if (t instanceof TimeoutException) {
+                        log.info("Idle timeout reached for flowable: ${id}")
+                        completeFlowable(id)
+                    } else {
+                        log.info("Unexpected error happened for id: ${id} | ${t.message}")
+                    }
+                } as Consumer
+        )
+    }
+
     Flowable getFlowable(Long id) {
         Flowable hotFlowable = flowableByIdCache[id]
 
         if (!hotFlowable) {
-            throw new NonExistingFlowableException("No flowable for workflow ${id} exists")
+            throw new NonExistingFlowableException("No flowable exists for id: ${id}")
         }
 
         hotFlowable
