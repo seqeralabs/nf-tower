81990c8fa89f14c87f2fd0727d8fda5a70b963cb
Paolo Di Tommaso <paolo.ditommaso@gmail.com>
2019-08-03 09:54:01 +0200

    Remove blanks

--- tower-web/src/main.ts
+++ tower-web/src/main.ts
@@ -1,7 +1,7 @@
 import { enableProdMode } from '@angular/core';
 import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
 
-import {MainModule} from "./app/modules/main/main.module";
+import { MainModule } from "./app/modules/main/main.module";
 import { environment } from './environments/environment';
 
 
