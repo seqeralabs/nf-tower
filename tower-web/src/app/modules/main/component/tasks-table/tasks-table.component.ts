b5917682d65622eebbbd39011ba51ef4d194963a
tcrespog <tomas.crespo@salenda.es>
2019-08-13 08:33:46 +0200

    Insert task status into a pill. Working on cell content scroll overflow

--- tower-web/src/app/modules/main/component/tasks-table/tasks-table.component.ts
+++ tower-web/src/app/modules/main/component/tasks-table/tasks-table.component.ts
@@ -63,10 +63,15 @@ export class TasksTableComponent implements OnInit, OnChanges {
       lengthChange: false,
       orderMulti: false,
       columns: [
-        {"name":"taskId"},{"name":"process"},{"name":"tag"},{"name":"status"},{"name":"hash"},{"name":"cpus"},{"name":"pcpu"},{"name":"memory"},{"name":"pmem"},{"name":"vmem"},
-        {"name":"rss"},{"name":"peakVmem"},{"name":"peakRss"},{"name":"time"},{"name":"duration"},{"name":"realtime"},{"name":"script"},{"name":"exit"},{"name":"submit"},
-        {"name":"start"},{"name":"complete"},{"name":"rchar"},{"name":"wchar"},{"name":"syscr"},{"name":"syscw"},{"name":"readBytes"},{"name":"writeBytes"},{"name":"nativeId"},
-        {"name":"name"},{"name":"module"},{"name":"container"},{"name":"disk"},{"name":"attempt"},{"name":"scratch"},{"name":"workdir"}],
+        {name: "taskId"},{name: "process"},{name: "tag"},
+        {name: "status", render: (data) => `<span class="badge badge-pill ${data.toLowerCase()}">${data}</span>`},
+        {name: "hash"},{name: "cpus"},{name: "pcpu"},{name: "memory"},{name: "pmem"},{name: "vmem"},
+        {name: "rss"},{name: "peakVmem"},{name: "peakRss"},{name: "time"},{name: "duration"},{name: "realtime"},
+        {name: "script", render: (data) => `<div class="scrollable">${data}</div>`},
+        {name: "exit"},{name: "submit"},
+        {name: "start"},{name: "complete"},{name: "rchar"},{name: "wchar"},{name: "syscr"},{name: "syscw"},{name: "readBytes"},{name: "writeBytes"},{name: "nativeId"},
+        {name: "name"},{name: "module"},{name: "container"},{name: "disk"},{name: "attempt"},{name: "scratch"},
+        {name: "workdir", render: (data) => `<div class="scrollable">${data}</div>`}],
       ajax: {
         url: this.workflowService.buildTasksGetUrl(this.workflowId),
         data: (data) => {
