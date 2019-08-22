ca5d3324d36748ce4b7cbcbdbd607c9111c0520a
Paolo Di Tommaso <paolo.ditommaso@gmail.com>
2019-08-22 12:13:17 +0200

    Update version number

--- tower-backend/src/main/groovy/io/seqera/tower/Application.groovy
+++ tower-backend/src/main/groovy/io/seqera/tower/Application.groovy
@@ -24,7 +24,7 @@ import io.swagger.v3.oas.annotations.info.License
 @OpenAPIDefinition(
     info = @Info(
             title = "Nextflow Tower",
-            version = "0.1",
+            version = "19.08.0",
             description = "Nextflow Tower service API",
             license = @License(name = "MPL 2.0", url = "https://www.mozilla.org/en-US/MPL/2.0/"),
             contact = @Contact(url = "http://seqera.io", name = "Paolo Di Tommaso", email = "p@seqera.io")
