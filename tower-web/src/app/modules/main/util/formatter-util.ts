44054681ecbebd408c787fb7b6952647466bee77
Paolo Di Tommaso <paolo.ditommaso@gmail.com>
2019-08-23 17:08:17 +0200

    Code cleanup

--- tower-web/src/app/modules/main/util/formatter-util.ts
+++ tower-web/src/app/modules/main/util/formatter-util.ts
@@ -14,10 +14,10 @@ import * as filesize from "file-size"
 
 export abstract class FormatterUtil {
 
+  // See https://www.npmjs.com/package/humanize-duration
   private static durationHumanizer: HumanizeDuration;
 
   static initialize() {
-    console.log('Initializing humanizer util');
     const language: HumanizeDurationLanguage  = new HumanizeDurationLanguage();
     language.addLanguage('short', <ILanguage> {y: () => 'y', mo: () => 'mo', w: () => 'w', d: () => 'd', h: () => 'h', m: () => 'm', s: () => 's'});
     this.durationHumanizer = new HumanizeDuration(language);
