9895ab1c7294ee238d9b80345ee4998f443f020a
Paolo Di Tommaso <paolo.ditommaso@gmail.com>
2019-09-06 18:41:57 +0200

    Change SSE logging to debug level

--- tower-backend/src/main/groovy/io/seqera/tower/controller/ServerSentEventsController.groovy
+++ tower-backend/src/main/groovy/io/seqera/tower/controller/ServerSentEventsController.groovy
@@ -1,23 +1,19 @@
 package io.seqera.tower.controller
 
+import javax.inject.Inject
+
 import groovy.util.logging.Slf4j
-import io.micronaut.context.annotation.Value
 import io.micronaut.http.annotation.Controller
 import io.micronaut.http.annotation.Get
 import io.micronaut.http.sse.Event
 import io.micronaut.security.annotation.Secured
 import io.micronaut.security.rules.SecurityRule
 import io.reactivex.Flowable
-import io.seqera.tower.domain.Workflow
 import io.seqera.tower.enums.SseErrorType
 import io.seqera.tower.exchange.trace.sse.TraceSseResponse
 import io.seqera.tower.service.ServerSentEventsService
 import io.seqera.tower.service.UserService
 import org.reactivestreams.Publisher
-
-import javax.inject.Inject
-import java.time.Duration
-
 /**
  * Server Sent Events endpoints to receive live updates in the client.
  *
@@ -40,7 +36,7 @@ class ServerSentEventsController {
 
     @Get("/workflow/{workflowId}")
     Publisher<Event<TraceSseResponse>> liveWorkflow(String workflowId) {
-        log.info("Subscribing to live events of workflow: ${workflowId}")
+        log.debug("Subscribing to live events of workflow: ${workflowId}")
         Flowable<Event<TraceSseResponse>> workflowFlowable
         try {
             workflowFlowable = serverSentEventsService.getOrCreateWorkflowPublisher(workflowId)
@@ -56,7 +52,7 @@ class ServerSentEventsController {
 
     @Get("/user/{userId}")
     Publisher<Event<TraceSseResponse>> liveUser(Long userId) {
-        log.info("Subscribing to live events of user: ${userId}")
+        log.debug("Subscribing to live events of user: ${userId}")
         Flowable<Event> userFlowable
         try {
             userFlowable = serverSentEventsService.getOrCreateUserPublisher(userId)
