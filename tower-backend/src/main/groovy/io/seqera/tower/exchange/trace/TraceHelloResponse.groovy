c735eba42551647151f9333bedd6af8ed66caa71
Paolo Di Tommaso <paolo.ditommaso@gmail.com>
2019-09-08 16:45:31 +0200

    Add trace hello verb

---
+++ tower-backend/src/main/groovy/io/seqera/tower/exchange/trace/TraceHelloResponse.groovy
@@ -0,0 +1,22 @@
+/*
+ * Copyright (c) 2019, Seqera Labs.
+ *
+ * This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at http://mozilla.org/MPL/2.0/.
+ *
+ * This Source Code Form is "Incompatible With Secondary Licenses", as
+ * defined by the Mozilla Public License, v. 2.0.
+ */
+
+package io.seqera.tower.exchange.trace
+
+import io.seqera.tower.exchange.BaseResponse
+
+/**
+ *
+ * @author Paolo Di Tommaso <paolo.ditommaso@gmail.com>
+ */
+class TraceHelloResponse implements BaseResponse {
+    String message
+}
