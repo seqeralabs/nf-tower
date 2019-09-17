73584e019c45c6e7a3c03a3adbd8203313c03857
tcrespog <tomas.crespo@salenda.es>
2019-09-17 07:36:02 +0200

    Fix duplicated connection error

--- tower-web/src/app/modules/main/service/live-events.service.ts
+++ tower-web/src/app/modules/main/service/live-events.service.ts
@@ -10,7 +10,7 @@
  */
 import { Injectable } from '@angular/core';
 import {environment} from "../../../../environments/environment";
-import {BehaviorSubject, Observable, ReplaySubject, Subject, Subscriber} from "rxjs";
+import {BehaviorSubject, Observable, Subject} from "rxjs";
 import {Workflow} from "../entity/workflow/workflow";
 import {User} from "../entity/user/user";
 import {LiveUpdate} from "../entity/live/live-update";
@@ -27,6 +27,7 @@ export class LiveEventsService {
   private connectionStatusSubject: BehaviorSubject<boolean>;
 
   private events$: Observable<LiveUpdate>;
+  private eventsSubject: Subject<LiveUpdate>;
 
   constructor() {
     this.connectionStatusSubject = new BehaviorSubject(null);
@@ -59,38 +60,41 @@ export class LiveEventsService {
   }
 
   private connect(url: string): Observable<LiveUpdate> {
-    return new Observable((subscriber: Subscriber<LiveUpdate>) => {
-      console.log('Connecting to receive live events', url);
-      const eventSource: EventSource = new EventSource(url);
+    this.eventsSubject = new Subject<LiveUpdate>();
+    this.events$ = this.eventsSubject.asObservable();
 
-      eventSource.addEventListener('open', () => {
-        console.log('Connection established', new Date().toISOString());
-        this.updateStatus(true);
-      });
+    console.log('Connecting to receive live events', url);
+    const eventSource: EventSource = new EventSource(url);
 
-      eventSource.addEventListener('message', (event: MessageEvent) => {
-        const dataArray: any[] = JSON.parse(event.data);
-        console.log(`Event at ${new Date().toISOString()} =>`, dataArray);
-        if (!dataArray || (Array.isArray(dataArray) && dataArray.length == 0)) {
-          return;
-        }
-
-        const events: LiveUpdate[] = dataArray.map((data) => new LiveUpdate(data));
-        events.forEach((event: LiveUpdate) => {
-          if (event.isError) {
-            subscriber.error(event);
-          } else {
-            subscriber.next(event)
-          }
-        });
-
-      });
-
-      eventSource.addEventListener('error', () => {
-        this.updateStatus(false);
-        console.log('Event source error', new Date().toISOString());
-      });
+    eventSource.addEventListener('open', () => {
+      console.log('Connection established', new Date().toISOString());
+      this.updateStatus(true);
     });
+
+    eventSource.addEventListener('message', (event: MessageEvent) => {
+      const dataArray: any[] = JSON.parse(event.data);
+      console.log(`Event at ${new Date().toISOString()} =>`, dataArray);
+      if (!dataArray || (Array.isArray(dataArray) && dataArray.length == 0)) {
+        return;
+      }
+
+      const events: LiveUpdate[] = dataArray.map((data) => new LiveUpdate(data));
+      events.forEach((event: LiveUpdate) => {
+        if (event.isError) {
+          this.eventsSubject.error(event);
+        } else {
+          this.eventsSubject.next(event)
+        }
+      });
+
+    });
+
+    eventSource.addEventListener('error', () => {
+      this.updateStatus(false);
+      console.log('Event source error', new Date().toISOString());
+    });
+
+    return this.events$;
   }
 
   private updateStatus(newStatus: boolean): void {
